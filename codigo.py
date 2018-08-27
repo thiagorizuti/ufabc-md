67f15ed00156d7785f5f5e47c702ef2834aa5cff
Thiago <t.rizuti@gmail.com>
2018-08-26 21:47:56 -0300

    relatorio final etapa 2

--- codigo.py
+++ codigo.py
@@ -1,14 +1,16 @@
 import librosa
+import os
 import pandas as pd
 import numpy as np
 import scipy as sp
-
-import os
-import time
-
-from sklearn.model_selection import GridSearchCV, PredefinedSplit
-from sklearn.metrics import classification_report, confusion_matrix
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 
 def read_audio(path):
     """
@@ -16,6 +18,7 @@ def read_audio(path):
     lê cada arquivo e divide o sinal do captcha a cada 2 segundos (quatro caracteres)
 
     path -- caminho para o diretorio contendo os arquivos .wav
+    
     retorna -- lista de caracteres na forma de tuplas (sinal, taxa de amostragem, rotulo do caractere, rotulo do captcha)
     """
     wavs = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file)) and file.endswith('.wav')]
@@ -23,27 +26,17 @@ def read_audio(path):
     for j,wav in enumerate(wavs):
         signal, sampling_rate = librosa.load(os.path.join(path,wav), None)
         labels = wav.split('.wav')[0]
-        chars += [(signal[sampling_rate*2*i:sampling_rate*2*(i+1)],sampling_rate,labels[i],labels) for i in range(4)]
+        chars += [(signal[sampling_rate*2*i:sampling_rate*2*(i+1)],sampling_rate,labels[i],path+'_'+labels) for i in range(4)]
     return chars
 
 
-def extract_features(chars, n_mfcc=20):
+def extract_features(chars):
     """
-    extrai n mfccs de cada caractere e computa as estatísticas:
-        *moda
-        *minimo
-        *maximo
-        *media
-        *desvio padrao
-        *mediana
-        *intervalo interquartil
-        *curtose
-        *assimetria
-
+    extrai mfccs de cada caractere e computa estatisticas básicas
+    
     chars -- lista de caracteres na forma de tuplas (sinal, taxa de amostragem, rotulo do caractere, rotulo do captcha)
-    n_mfcc -- numero de mfccs a serem extraidos
 
-    retorna -- tabela atributo valor (linhas: len(caracteres), colunas: n_mfcc*9 + char_label + captcha_label)
+    retorna -- tabela atributo valor
     """
     data = pd.DataFrame()
     for j, char in enumerate(chars):
@@ -54,7 +47,7 @@ def extract_features(chars, n_mfcc=20):
         row = pd.DataFrame()
         row['char_label'] = [char_label]
         row['captcha_label'] = [captcha_label]
-        mfcc = librosa.feature.mfcc(signal,sampling_rate, n_mfcc=n_mfcc)
+        mfcc = librosa.feature.mfcc(signal,sampling_rate, n_mfcc=40)
         for i,mfcc in enumerate(mfcc):
             row['mfcc_'+str(i)+'_mode'] = [np.mean(sp.stats.mode(mfcc))]
             row['mfcc_'+str(i)+'_min'] = [np.min(mfcc)]
@@ -69,68 +62,92 @@ def extract_features(chars, n_mfcc=20):
     return data.reset_index(drop=True)
 
 
-def train_model(train_data,valid_data,transformer,estimators,param_grids,metric):
+def select_model(data):
     """
     busca o melhor modelo e combinação de hiperparâmetros
 
-    train_data -- dados de treinamento
-    valid_data -- dados de validacao
-    estimators -- lista de modelos
-    param_grid -- lista de dicionarios de hiperparâmetros
+    data -- dados para validação cruzada
 
-    retorna -- transformador ajustado, melhor modelo treinado, tabela de resultados
+    retorna -- ensemble dos melhores modelos
     """
-    X_train = train_data.drop(['captcha_label','char_label'],axis=1)
-    y_train = train_data['char_label']
-
-    X_valid = valid_data.drop(['captcha_label','char_label'],axis=1)
-    y_valid = valid_data['char_label']
-
-    X_train = pd.DataFrame(transformer.fit_transform(X_train),columns=X_train.columns)
-    X_valid = pd.DataFrame(transformer.transform(X_valid),columns=X_valid.columns)
-
-    X = np.concatenate((X_train.values,X_valid.values),axis=0)
-    y = np.concatenate((y_train.values,y_valid.values),axis=0)
-    test_fold = []
-    for i in range(len(X_train)):
-        test_fold.append(-1)
-    for i in range(len(X_valid)):
-        test_fold.append(0)
-    cv = PredefinedSplit(test_fold=test_fold)
-
-    metrics = {'estimator':[],'training metric':[],'validation metric':[]}
-    best_estimator = None
-    best_score = 0
-    for estimator,param_grid in zip(estimators,param_grids):
-        gridsearch = GridSearchCV(estimator,param_grid,scoring=metric,cv=cv,return_train_score=True)
-        gridsearch.fit(X,y)
-        metrics['estimator'].append(str(gridsearch.best_estimator_).split('(')[0])
-        metrics['validation metric'].append(gridsearch.best_score_)
-        results = pd.DataFrame(gridsearch.cv_results_)
-        metrics['training metric'].append(results[results.rank_test_score == 1]['mean_train_score'].values[0])
-        if gridsearch.best_score_ > best_score:
-            best_score = gridsearch.best_score_
-            best_estimator = gridsearch.best_estimator_
-    best_estimator.fit(X_train,y_train)
-    return transformer, best_estimator, pd.DataFrame(metrics)
-
-def evaluation_per_character(transformer,estimator,data):
+    seed=42 
+    
     X = data.drop(['captcha_label','char_label'],axis=1)
     y = data['char_label']
-    X = pd.DataFrame(transformer.transform(X),columns=X.columns)
-    y_pred = estimator.predict(X)
-    metrics = classification_report(y,y_pred)
-    conf = pd.DataFrame(confusion_matrix(y,y_pred,labels=y.unique()),columns=y.unique(),index=y.unique())
-    return metrics, conf
 
-def evaluation_per_captcha(transformer,estimator,data):
-    accuracy = 0
+    estimators = [Pipeline(steps=[('scaler', StandardScaler())
+                                  ,('selector',SelectKBest(score_func=mutual_info_classif)),
+                                  ('model',KNeighborsClassifier())]),
+              Pipeline(steps=[('scaler', StandardScaler()),
+                              ('model',LogisticRegression())]),
+              Pipeline(steps=[('model',RandomForestClassifier())])
+                 ]
+                  
+    param_grids = [{'selector__k':[50,75,100],
+                    'model__n_neighbors':[2,3,5], 
+                    'model__weights':['distance'],
+                    'model__p':[1,2]},
+                   
+               {'model__C':np.logspace(-4,4,9),
+                'model__penalty':['l1','l2'],
+                'model__random_state':[seed]},
+                   
+               {'model__max_depth':np.arange(2,20),
+                'model__n_estimators':[500],
+                'model__random_state':[seed]}]
+
+    best_score = 0
+    best_estimators=[]
+    for estimator,param_grid in zip(estimators,param_grids):
+        gridsearch = GridSearchCV(estimator,param_grid,scoring='accuracy',cv=5,error_score=0,verbose=True)
+        gridsearch.fit(X,y)
+        best_estimators.append(gridsearch.best_estimator_)
+    final_estimator = VotingClassifier([('knn',best_estimators[0]),('lr',best_estimators[1]),('rf',best_estimators[2])],voting='soft')
+    return final_estimator
+
+def train_model(estimator,data):
+    """
+    treina o modelo no conjunto de dados
+
+    estimator -- modelo a ser treinado
+    
+    data -- dados de treinamento
+
+    retorna -- modelo treinado
+    """
+    X = data.drop(['captcha_label','char_label'],axis=1)
+    y = data['char_label']
+    estimator.fit(X,y)
+    return estimator
+
+def evaluate_model(estimator,data):
+    """
+    imprime report de avaliação do modelo no conjunto de dados
+    
+    estimator -- modelo 
+    
+    data -- dados de treinamento
+    
+    """
+    X = data.drop(['captcha_label','char_label'],axis=1)
+    y = data['char_label']
+    y_pred = estimator.predict(X)
+    accuracy = accuracy_score(y,y_pred)
+    metrics = classification_report(y,y_pred)
+    conf = pd.DataFrame(confusion_matrix(y,y_pred,labels=y.unique()),columns=y.unique(),index=y.unique())      
+    hits = 0
     captchas = data['captcha_label'].unique()
     for captcha in captchas:
         X = data[data.captcha_label==captcha].drop(['captcha_label','char_label'],axis=1)
         y = data[data.captcha_label==captcha]['char_label']
-        X = pd.DataFrame(transformer.transform(X),columns=X.columns)
         y_pred = estimator.predict(X)
         if np.equal(y_pred,y).sum() == 4:
-            accuracy+=1
-    return accuracy/len(captchas)
+            hits+=1
+    print('confusion matrix' + '\n')
+    print(conf)
+    print('\n')
+    print(metrics)
+    print('\n' + 'accuracy {0:.2f}'.format(accuracy) + '\n')
+    print('\n' + 'captchas hit rate ' + "{0:.2f}".format(hits/len(captchas)))
+
+
